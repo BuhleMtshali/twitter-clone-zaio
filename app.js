42b094e7b6cdca90fa7264617a7c43b36834bb83
Buhle <mtshalizanobuhle7@gmail.com>
2025-01-04 16:15:33 +0200

    updated the stylesheet

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-//function toggling active nav bar elements
-let iconTextDivs = document.querySelectorAll('.icon-text');
-iconTextDivs.forEach((div) => {
-    div.addEventListener('click', () => {
-        console.log(div)
-        
-    })
-})
 
