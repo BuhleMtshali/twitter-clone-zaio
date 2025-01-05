7e542fbf8dc75065a55f9e4cc205456b253d408c
Buhle <mtshalizanobuhle7@gmail.com>
2025-01-05 13:09:10 +0200

    upodated the javascript file

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-
+//function for showing the small text on the post feed
+let postInput = document.querySelector('#post-input');
+postInput.addEventListener('click', () => {
+    let smallText = document.querySelector('#small-text');
+    smallText.style.display = 'block'
+})
